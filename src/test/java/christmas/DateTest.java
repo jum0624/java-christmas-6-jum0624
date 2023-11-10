43a81da321c945c599fd10c04df92d21344f928e
jum0624 <jum0624@naver.com>
2023-11-10 23:18:40 +0900

    test: 날짜 기능 구현 테스트

---
+++ src/test/java/christmas/DateTest.java
@@ -0,0 +1,38 @@
+package christmas;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DateTest {
+    @Test
+    @DisplayName("1일 ~ 31일 이외의 숫자 입력 시, 예외가 발생한다.")
+    public void inputDateRangeExceptionTest() throws Exception {
+        // given
+        int day1 = 0;
+        int day2 = 32;
+
+        // when
+        // then
+        Assertions.assertThatThrownBy(() -> new Date(day1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1일 ~ 31일 이내의 값만 입력해주세요.");
+
+        Assertions.assertThatThrownBy(() -> new Date(day2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1일 ~ 31일 이내의 값만 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("날짜를 입력하면 요일 정보를 가져올 수 있다.")
+    public void getDayOfWeekTest() throws Exception {
+        // given
+        int day = 24;
+
+        // when
+        Date date = new Date(day);
+
+        // then
+        Assertions.assertThat(date.findDateOfWeek()).isEqualTo(7);
+    }
+}
