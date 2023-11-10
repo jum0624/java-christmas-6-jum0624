ed9937dec47e0ae2e68d305a6cec29dc1d29465e
jum0624 <jum0624@naver.com>
2023-11-11 03:55:02 +0900

    test: 메뉴 존재 여부 테스트

---
+++ src/test/java/christmas/MenuTest.java
@@ -0,0 +1,20 @@
+package christmas;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MenuTest {
+    @Test
+    @DisplayName("주문한 메뉴가 존재하는지 확인한다.")
+    public void checkExistMenuTest() throws Exception {
+        // given
+        String menu = "양송이수프";
+
+        // when
+        Menu orderedMenu = Menu.findByMenuName(menu);
+
+        // then
+        Assertions.assertThat(orderedMenu).isEqualTo(Menu.MENU1);
+    }
+}
