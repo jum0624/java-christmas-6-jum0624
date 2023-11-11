5d0ef24730c2906ca0f2c0fb74d47de829f0a920
jum0624 <jum0624@naver.com>
2023-11-11 22:34:03 +0900

    add: 주문 관련 상수 추가 및 ERROR 메세지 추가

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -1,7 +1,9 @@
 package christmas.constant;
 
 public enum ExceptionMessage {
+    ERROR("[ERROR] "),
     ERROR_INPUT_ORDER_MESSAGE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ERROR_INPUT_ORDER_MENU_COUNT_MESSAGE("메뉴의 개수는 1 이상의 숫자만 입력되도록 해주세요."),
     ERROR_INPUT_NUMBER_TYPE_MESSAGE("숫자만 입력해주세요."),
     ERROR_INPUT_DATE_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     private String message;
@@ -11,6 +13,6 @@ public enum ExceptionMessage {
     }
 
     public String getMessage() {
-        return message;
+        return ERROR.message + message;
     }
 }
--- src/main/java/christmas/constant/NumberConstant.java
+++ src/main/java/christmas/constant/NumberConstant.java
@@ -4,7 +4,9 @@ public enum NumberConstant {
     MIN_DAY(1),
     MAX_DAY(31),
     YEAR(2023),
-    MONTH(12);
+    MONTH(12),
+    MIN_MENU_COUNT(1),
+    MAX_MENU_COUNT(20);
     private int number;
 
     NumberConstant(int number) {
