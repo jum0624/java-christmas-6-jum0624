3d667b0e6419b240a9cda4b4abcaabce82e3611d
jum0624 <jum0624@naver.com>
2023-11-13 03:49:49 +0900

    refactor: 예외처리 수정
    숫자 이외의 값이 입력으로 들어왔을 때, 예외 메세지 변경 및 삭제

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -4,7 +4,6 @@ public enum ExceptionMessage {
     ERROR("[ERROR] "),
     ERROR_INPUT_ORDER_MESSAGE("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ERROR_INPUT_ORDER_MENU_COUNT_MESSAGE("메뉴의 개수는 1 이상의 숫자만 입력되도록 해주세요."),
-    ERROR_INPUT_NUMBER_TYPE_MESSAGE("숫자만 입력해주세요."),
     ERROR_INPUT_DATE_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     private String message;
 
