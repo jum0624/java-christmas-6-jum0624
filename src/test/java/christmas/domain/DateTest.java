f4a7fac9f2b7505512013bc30d5e5e212d9737d5
jum0624 <jum0624@naver.com>
2023-11-15 03:23:54 +0900

    refactor: 테스트 코드 패키지 변경

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -1,6 +1,5 @@
-package christmas;
+package christmas.domain;
 
-import christmas.domain.Date;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
